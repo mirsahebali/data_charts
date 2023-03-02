e70ba01c7db1b8dc4445e8e6648323fef343bd6f
Mir Saheb Ali <mirsahebali204@gmail.com>
2023-03-02 21:41:31 +0530

    commit 2

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+function Header() {
+          return ( <div className="flex justify-between px-4 pt-4">
+                 <h2>Dashboard</h2>
+                 <h2>Welcome back, Saheb</h2>   
+          </div> );
+}
+
+export default Header;
